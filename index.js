01a5c8e9c2cc38bcb1e64f782f985ddde53b81a8
“gastonninin” <gastonninin@gmail.com>
2018-08-24 12:31:04 -0300

    Desafio 3

---
+++ index.js
@@ -0,0 +1,25 @@
+var operacion = prompt('¿Qué operación desea realizar?');
+var num1 = prompt('Ingrese un valor');
+var num2 = prompt('Ingrese un segundo valor');
+var sum = num1 + num2;
+
+if(operacion = 'sum') {
+	console.log("El resultado de la suma es " + (num1+num2));
+} 
+
+if(operacion = 'res') {
+	console.log("El resultado de la resta es " + (num1-num2));
+} 
+
+if(operacion = 'mul') {
+	console.log("El resultado de la multiplicación es " + (num1*num2));
+} 
+
+if(operacion = 'div') {
+	console.log("El resultado de la división es " + (num1/num2))
+	do {
+        alert("Ingrese otro valor distinto de 0");
+        var num2 = prompt("Ingrese otro valor");
+    } while(num2 = 0)
+	console.log("El resultado de la división es " + (num1/num2));
+}
